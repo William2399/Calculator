37a389c5c279bc2f2487ef2537f0a06173f48924
William Guo <williamguo2399@gmail.com>
2023-01-16 17:50:45 -0800

    Edit index.html and Add comments to main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Declare and initialize constants and variables
 const numbers = document.querySelectorAll('[data-number]');
 const decimal = document.querySelector('[data-decimal]')
 const operations = document.querySelectorAll('[data-operation]');
@@ -8,24 +9,29 @@ const equals = document.querySelector('[data-equals]');
 const previous_expressionText = document.querySelector('[data-previous-expression]');
 const current_expressionText = document.querySelector('[data-current-expression]');
 
+//Calculator class simulates the basic capabilties of a calcualtor
 class Calculator {
 
+    //Constructor for class
     constructor(previous_expressionText, current_expressionText){
         this.previous_expressionText = previous_expressionText;
         this.current_expressionText = current_expressionText;
         this.resetCalculator();
     }
 
+    //Clears the calculator
     resetCalculator(){
         this.previous_expression = '';
         this.current_expression = '';
         this.operation = null;
     }
 
+    //Delete the most recent added number
     delete(){
         this.current_expression = this.current_expression.toString().slice(0, -1);
     }
 
+    //Change the sign of the current number
     changeSign(){
         if (this.current_expression !== ''){
             let temp = parseFloat(this.current_expression) * -1;
@@ -33,17 +39,21 @@ class Calculator {
         }
     }
 
+    //Add a number to the current expression
     addNumber(number){
         this.current_expression = this.current_expression + number;
     }
 
+    //Add a decimal to the current expression
     addDecimal(){
         if (this.current_expression.toString().includes('.')){return;}
         this.current_expression = this.current_expression + '.';
     }
 
+    //Select an operator to use in an operation
     selectOperator(operation){
-        if (this.current_expression === '' || this.current_expression === '.'){return;}
+        if (this.current_expression === '' || 
+            this.current_expression === '.'){return;}
         if (this.previous_expression !== ''){
             this.calculate();
         }
@@ -52,6 +62,7 @@ class Calculator {
         this.current_expression = '';
     }
 
+    //Calculate the given operation between prev and curr expression
     calculate(){
         let previous = parseFloat(this.previous_expression);
         let current = parseFloat(this.current_expression)
@@ -85,6 +96,7 @@ class Calculator {
         this.operation = null;
     }
 
+    //Update the display of the calculator
     updateCalculator(){
         this.current_expressionText.innerText = this.current_expression;
         if (this.operation !== null){
@@ -96,6 +108,7 @@ class Calculator {
     }
 }
 
+//Declare and initialize a new object of Calculator class
 const calculator = new Calculator(previous_expressionText, current_expressionText);
 
 numbers.forEach(number => {
